429bf7cdd21554911b7fd697ac668ed054ad85a0
SeraphimAnokhin <138584469+SeraphimAnokhin@users.noreply.github.com>
2026-03-29 22:05:50 +0300

    Create gen.h

---
+++ genetic/gen.h
@@ -0,0 +1,31 @@
+#ifndef GEN_H
+#define GEN_H
+
+#include <random>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+class Knapsack_task {
+    int k;
+    int n;
+    int *w, *v;
+public:
+    Knapsack_task(int k_, int n_, int *w_, int *v_);
+    Knapsack_task(const Knapsack_task &t);
+    ~Knapsack_task();
+    inline int get_n() const {return n;}
+    int eval(bool *arr) const;
+    int eval(std::vector<bool> arr) const;
+};
+
+
+class Solver {
+public:
+    int solve(const Knapsack_task &t, bool *opt_arr);
+};
+
+
+
+#endif
